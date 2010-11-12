f518c656da28d972511543800b60f02ca28e11a6
Takuya ASADA <syuu@dokukino.com>
2010-11-12 19:22:59 +0900

    license added

--- native/com_dokukino_rados4j_Rados.cpp
+++ native/com_dokukino_rados4j_Rados.cpp
@@ -1,12 +1,68 @@
+/*******************************************************************************
+*Copyright (c) 2010  Takuya ASADA
+* 
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, only version 3 of the License.
+* 
+* 
+*  This file is distributed in the hope that it will be useful, but WITHOUT
+*  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+*  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+*  for more details.
+* 
+*  You should have received a copy of the GNU General Public License along
+*  with this program.  If not, see <http://www.gnu.org/licenses/>.
+* 
+*  Please contact Eucalyptus Systems, Inc., 130 Castilian
+*  Dr., Goleta, CA 93101 USA or visit <http://www.eucalyptus.com/licenses/>
+*  if you need additional information or have any questions.
+* 
+*  This file may incorporate work covered under the following copyright and
+*  permission notice:
+* 
+*    Software License Agreement (BSD License)
+* 
+*    Copyright (c) 2008, Regents of the University of California
+*    All rights reserved.
+* 
+*    Redistribution and use of this software in source and binary forms, with
+*    or without modification, are permitted provided that the following
+*    conditions are met:
+* 
+*      Redistributions of source code must retain the above copyright notice,
+*      this list of conditions and the following disclaimer.
+* 
+*      Redistributions in binary form must reproduce the above copyright
+*      notice, this list of conditions and the following disclaimer in the
+*      documentation and/or other materials provided with the distribution.
+* 
+*    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+*    IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+*    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+*    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+*    OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+*    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+*    PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+*    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+*    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+*    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+*    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. USERS OF
+*    THIS SOFTWARE ACKNOWLEDGE THE POSSIBLE PRESENCE OF OTHER OPEN SOURCE
+*    LICENSED MATERIAL, COPYRIGHTED MATERIAL OR PATENTED MATERIAL IN THIS
+*    SOFTWARE, AND IF ANY SUCH MATERIAL IS DISCOVERED THE PARTY DISCOVERING
+*    IT MAY INFORM DR. RICH WOLSKI AT THE UNIVERSITY OF CALIFORNIA, SANTA
+*    BARBARA WHO WILL THEN ASCERTAIN THE MOST APPROPRIATE REMEDY, WHICH IN
+*    THE REGENTS’ DISCRETION MAY INCLUDE, WITHOUT LIMITATION, REPLACEMENT
+*    OF THE CODE SO IDENTIFIED, LICENSING OF THE CODE SO IDENTIFIED, OR
+*    WITHDRAWAL OF THE CODE CAPABILITY TO THE EXTENT NEEDED TO COMPLY WITH
+*    ANY SUCH LICENSES OR RIGHTS.
+*******************************************************************************/
 #include "com_dokukino_rados4j_Rados.h"
 #include <rados/librados.hpp>
 #include <iostream>
 
 static jfieldID rados_instance_ptr = NULL;
-static const char **rados_argv = NULL;
-static int rados_argc = 0;
-static jstring *rados_strs = NULL;
-static jobjectArray rados_args = NULL;
 
 extern jclass pool_class;
 extern jfieldID pool_instance_ptr, pool_rados_ptr;
@@ -77,7 +133,6 @@ extern "C"
     {
         jint ret;
         librados::Rados *rados;
-
         ret = rados_field_initialize(env);
         if (ret)
             return -1;
@@ -98,19 +153,7 @@ extern "C"
         if (!rados)
             return -1;
 
-	rados_args = args;
-	rados_argc = env->GetArrayLength(args);
-	if (rados_argc > 0) {
-		rados_argv = new const char*[rados_argc];
-		rados_strs = new jstring[rados_argc];
-		for (int i = 0; i < rados_argc; i++) {
-			rados_strs[i] = (jstring)env->GetObjectArrayElement(args, i);
-			rados_argv[i] = env->GetStringUTFChars(rados_strs[i], NULL);
-			printf("[%d] %s\n", i, rados_argv[i]);
-		}
-	        ret = rados->initialize(rados_argc, rados_argv);
-	}else
-	        ret = rados->initialize(0, NULL);
+        ret = rados->initialize(0, NULL);
         rados_set_instance_ptr(env, obj, rados);
         return ret;
     }
@@ -131,13 +174,6 @@ extern "C"
         rados->shutdown();
         delete rados;
 	rados_set_instance_ptr(env, obj, NULL);
-	if (rados_argv) {
-		for (int i = 0; i < rados_argc; i++) {
-			env->ReleaseStringUTFChars(rados_strs[i], rados_argv[i]);
-		}
-		delete rados_argv;
-		delete rados_strs;
-	}
     }
 
     /*
